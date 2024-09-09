b3f34ab9751f100afdd24e942dd1aa8ca53cca60
Valorie Portem <valorieportem@gmail.com>
2024-09-09 11:57:16 +0800

    initial commit

---
+++ variable.js
@@ -0,0 +1,8 @@
+let name = "Valorie";
+let whatDoYouWannaBecomeInYourLife = "A programmer";
+let gender = "Female";
+let twitterHandle = "@reve_riy";
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
